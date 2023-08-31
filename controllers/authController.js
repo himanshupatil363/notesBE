ea19f1b867d76aabf379d514ae2851a048f4eecb
Himanshu Patil <patilhnofficial@gmail.com>
2023-08-31 17:36:16 +0530

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const generateAuthToken = (id) => {
   const token = jwt.sign({ user: id }, process.env.SECRET_KEY, {
--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,5 +1,5 @@
-const Note = require("../models/Note");
-const User = require("../models/User");
+const Note = require("../models/note");
+const User = require("../models/user");
 
 // Controller to create a new note
 const createNote = async (req, res) => {
